d5d9a1cd9e6800b6f39ddc8596272dce0af14cfa
Trevor_143 <trevorarapu2@gmail.com>
2023-11-08 11:31:24 +0300

    testing tinyMCE

---
+++ app.vue
@@ -0,0 +1,47 @@
+<!-- <template>
+  <div>
+    <NuxtWelcome />
+  </div>
+</template> -->
+
+<script setup>
+  import Editor from '@tinymce/tinymce-vue'
+</script>
+
+<template>
+  <main id="sample">
+    <Editor
+      api-key="bfjpt8qfnb40qreusjb04wksnahw2kyzigzjjxwr990019y7"
+      :init="{
+        toolbar_mode: 'sliding',
+        plugins: 'ai tinycomments mentions anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount checklist mediaembed casechange export formatpainter pageembed permanentpen footnotes advtemplate advtable advcode editimage tableofcontents mergetags powerpaste tinymcespellchecker autocorrect a11ychecker typography inlinecss',
+        toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | align lineheight | tinycomments | checklist numlist bullist indent outdent | emoticons charmap | removeformat',
+        tinycomments_mode: 'embedded',
+        tinycomments_author: 'Author name',
+        mergetags_list: [
+          { value: 'First.Name', title: 'First Name' },
+          { value: 'Email', title: 'Email' },
+        ],
+        ai_request: (request, respondWith) => respondWith.string(() => Promise.reject(' See docs to implement AI Assistant ')),
+      }"
+      initial-value="Welcome to TinyMCE!"
+    />
+  </main>
+</template>
+
+<style scoped>
+
+#sample {
+  width: 100%;
+  margin: 1rem 0.5rem;
+}
+
+@media (min-width: 1024px) {
+  #sample {
+    display: flex;
+    flex-direction: column;
+    place-items: center;
+    width: 100%;
+  }
+}
+</style>
